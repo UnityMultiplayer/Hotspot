0afbd7a80457562df36c311d47ab9ec23c309b32
Storm Anderson <storm@artrinix.xyz>
2025-07-16 23:49:30 +1200

    #9 UI Overhaul

--- app.vue
+++ app.vue
@@ -8,20 +8,9 @@
 </template>
 
 <style lang="scss">
-  /* I don't like this. ~Storm */
-  @import url('https://gists.rawgit.com/mfd/f3d96ec7f0e8f034cc22ea73b3797b59/raw/856f1dbb8d807aabceb80b6d4f94b464df461b3e/gotham.css');
-  @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap');
-
-  @theme {
-    --font-sans: Manrope, GothamPro, Roboto, 'Segoe UI', sans-serif;
-  }
-
   body {
-    //text-shadow: 1px 1px 1px var(--text-shadow);
-
     background-color: var(--bg-main);
     color: var(--text-main);
-    //font-family: GothamPro, Roboto, 'Segoe UI', sans-serif;
   }
 
   main {
@@ -51,32 +40,25 @@
 
   /* Light mode */
   :root {
-    --bg-checker-color1: #f7f6f2;
-    --bg-checker-color2: #e8eaeb;
+    --bg-main: var(-color-zinc-50);
 
-    --bg-main: #ecf9fb;
-    --bg-gradient-color1: #0059FF7F;
-    --bg-gradient-color2: #00CCFF7F;
-
-    --text-title: #24292f;
-    --text-main: #6e7781;
-    --text-buttons: #e3e8f0;
-    --text-live: #FF4848;
+    --text-title: var(--color-zinc-950);
+    --text-shadow: rgba(255, 255, 255, 0.5);
+    --text-main: var(--color-zinc-900);
+    --button-background: var(--color-zinc-100);
+    --text-live: var(--color-rose-500);
   }
 
   /* Dark mode */
   @media (prefers-color-scheme: dark) {
     :root {
-      --bg-checker-color1: #181a1f;
-      --bg-checker-color2: #16181c;
+      --bg-main: var(--color-zinc-900);
 
-      --bg-main: #16181c;
-      --bg-gradient-color1: #0059FF7F;
-      --bg-gradient-color2: #00CCFF7F;
-
-      --text-title: #ecf9fb;
-      --text-main: #b0bac5;
-      --text-buttons: #26292f;
+      --text-title: var(--color-zinc-50);
+      --text-shadow: rgba(0, 0, 0, 0.8);
+      --text-main: var(--color-zinc-100);
+      --button-background: var(--color-zinc-950);
+      --text-live: var(--color-rose-400);
     }
   }
 
@@ -89,7 +71,7 @@
   }
 
   .unity-button {
-    background-color: var(--text-buttons);
+    background-color: var(--button-background);
   }
 </style>
 
--- components/members/MemberCard.vue
+++ components/members/MemberCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div :class="`w-2/3 md:w-1/3 lg:w-1/4 2xl:w-1/5 p-4 flex unity-button rounded-2xl border-[1px] m-0.5 ${isLive ? 'saturate-100' : 'saturate-40'} ${isLive ? 'brightness-100' : 'brightness-80'} transition-all hover:saturate-100 hover:brightness-[110%]`" :style="borderStyle" :title="member.id">
+  <div :class="`w-2/3 md:w-1/3 lg:w-1/4 2xl:w-1/5 p-4 flex unity-button rounded-2xl border-[1px] m-0.5 ${isLive ? 'saturate-100' : 'saturate-80 dark:saturate-70'} ${isLive ? 'brightness-100' : 'brightness-90 dark:brightness-70'} transition-all hover:saturate-100 hover:brightness-[110%]`" :style="borderStyle" :title="member.id">
     <img class="rounded-lg h-12" :src="`/img/creators/${member.id}.webp`" :alt="member.name">
     <div class="px-4">
       <span :style="`color: ${faction.color}`" class="font-medium text-shadow-sm not-dark:brightness-80">{{ member.name }}</span>
       &nbsp;
-      <span class="font-light text-sm">{{ member.pronouns.toLowerCase() }}</span>
+      <span class="font-light font-mono text-sm">{{ member.pronouns.toLowerCase() }}</span>
       <br>
       <span class="text-sm">{{ member.role }}</span>
 
